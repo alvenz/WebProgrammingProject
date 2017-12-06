9b121d05ecf3d79d5caa6c301ee13d2a48ef555e
Veelio <alvenzleonardus@gmail.com>
2017-12-06 07:14:55 +0700

    DB

---
+++ prk.sql
@@ -0,0 +1,371 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 06, 2017 at 01:14 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carts`
+--
+
+CREATE TABLE `carts` (
+  `cartId` int(10) UNSIGNED NOT NULL,
+  `userId` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `carts`
+--
+
+INSERT INTO `carts` (`cartId`, `userId`, `created_at`, `updated_at`) VALUES
+(1, 1, NULL, NULL),
+(2, 3, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_details`
+--
+
+CREATE TABLE `cart_details` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `cartId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `purchaseDate` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `cart_details`
+--
+
+INSERT INTO `cart_details` (`id`, `cartId`, `productId`, `qty`, `purchaseDate`, `created_at`, `updated_at`) VALUES
+(1, 1, 4, 3, '2017-12-05', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games_libraries`
+--
+
+CREATE TABLE `games_libraries` (
+  `gamesLibraryId` int(10) UNSIGNED NOT NULL,
+  `userId` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `games_libraries`
+--
+
+INSERT INTO `games_libraries` (`gamesLibraryId`, `userId`, `created_at`, `updated_at`) VALUES
+(1, 1, NULL, NULL),
+(2, 3, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games_library_details`
+--
+
+CREATE TABLE `games_library_details` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `gamesLibraryId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `games_library_details`
+--
+
+INSERT INTO `games_library_details` (`id`, `gamesLibraryId`, `productId`, `created_at`, `updated_at`) VALUES
+(1, 1, 2, NULL, NULL),
+(2, 1, 4, NULL, NULL),
+(3, 2, 1, NULL, NULL),
+(4, 2, 4, NULL, NULL),
+(5, 2, 3, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `genreTypeName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`id`, `genreTypeName`, `created_at`, `updated_at`) VALUES
+(1, 'FPS', NULL, NULL),
+(2, 'Sports', NULL, NULL),
+(3, 'Action', NULL, NULL),
+(4, 'Racing', NULL, NULL),
+(5, 'Simulation', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`migration`, `batch`) VALUES
+('2014_10_12_000000_create_users_table', 1),
+('2014_10_12_100000_create_password_resets_table', 1),
+('2017_12_03_222925_create_products_table', 2),
+('2017_12_04_225410_create_genres_table', 3),
+('2017_12_05_194950_create_transaction_histories_table', 4),
+('2017_12_05_200915_create_games_libraries_table', 5),
+('2017_12_05_200928_create_games_library_details_table', 5),
+('2017_12_05_233623_create_carts_table', 6),
+('2017_12_05_233632_create_cart_details_table', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `genreTypeId` int(11) NOT NULL,
+  `release_date` date NOT NULL,
+  `picture` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `rating` double(8,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `genreTypeId`, `release_date`, `picture`, `rating`, `created_at`, `updated_at`) VALUES
+(1, 'Battle Field 1', 450000, 1, '2017-12-04', 'battlefield_1.png', 2.00, NULL, '2017-12-04 14:16:03'),
+(2, 'The Sims 4', 400000, 5, '2017-12-04', 'the_sims_4.jpg', 2.00, NULL, NULL),
+(3, 'Fifa 18', 563000, 2, '2017-12-04', 'fifa_18.jpg', 2.00, NULL, NULL),
+(4, 'Battle Field 4', 125000, 1, '2017-12-04', 'battlefield_4.jpg', 2.00, NULL, NULL),
+(5, 'Need For Speed', 250000, 4, '2017-12-04', 'need_for_speed.jpg', 2.00, NULL, NULL),
+(6, 'Star Wars Battlefront 2', 768000, 3, '2017-12-04', 'star_wars_battlefront_2.jpg', 2.00, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_histories`
+--
+
+CREATE TABLE `transaction_histories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `userId` int(11) NOT NULL,
+  `transactionDate` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `transaction_histories`
+--
+
+INSERT INTO `transaction_histories` (`id`, `userId`, `transactionDate`, `created_at`, `updated_at`) VALUES
+(1, 1, '2017-12-06', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `dob` date NOT NULL,
+  `role` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `picture` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `dob`, `role`, `picture`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Alven Gemini Julio', 'alven@gmail.com', '$2y$10$DQeAqRy8PT6Wdyw8x92jO.LqwIJpWmnW87WdQlA5ZBc0janWWMYPy', '1996-06-03', 'Member', 'Lee_Hyun.jpg', '0wryK3TfJrlz9FGOs4OoETilE2HCIlMCFeI8dXObQLS91CeYoUmeNjoYnkzW', '2017-11-11 11:15:38', '2017-12-05 15:48:59'),
+(2, 'Admin', 'admin@admin.com', '$2y$10$ht58MGHRTkChWzy1qlNyA.3O3Bf8NTU67WrlYRY48ehwwQFQ6/glS', '1998-12-04', 'Admin', 'Lee_Hyun.jpg', 'RaooEGZypvMoqkbGGiqCTIEPOye6pbBUFRa8Ni1GqsANVvaVqEiCVSCKDznk', '2017-12-03 14:29:09', '2017-12-05 13:20:41'),
+(3, 'asd', 'asd@gmail.com', '$2y$10$q6wX4uiF1Aaitn83C73s3ODfWamWTiwqPP.AgUQJf1Uv4EsMNm/Km', '2017-12-06', 'Member', 'Lee_Hyun.jpg', '2mgDvM2ARC9mTeXDEJicVvvruEvEhnZ89cHPLW2vqya4Kiz8B4ezDKNFt6cu', '2017-12-05 13:49:12', '2017-12-05 15:48:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`cartId`);
+
+--
+-- Indexes for table `cart_details`
+--
+ALTER TABLE `cart_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `games_libraries`
+--
+ALTER TABLE `games_libraries`
+  ADD PRIMARY KEY (`gamesLibraryId`);
+
+--
+-- Indexes for table `games_library_details`
+--
+ALTER TABLE `games_library_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`),
+  ADD KEY `password_resets_token_index` (`token`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaction_histories`
+--
+ALTER TABLE `transaction_histories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `carts`
+--
+ALTER TABLE `carts`
+  MODIFY `cartId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `cart_details`
+--
+ALTER TABLE `cart_details`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `games_libraries`
+--
+ALTER TABLE `games_libraries`
+  MODIFY `gamesLibraryId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `games_library_details`
+--
+ALTER TABLE `games_library_details`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `transaction_histories`
+--
+ALTER TABLE `transaction_histories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
